f10a98dcecf18b960a0b4f50ae2b3ad6bdb58e9c
陈依娜 <haneyuzuru@yahoo.com>
2016-10-31 20:11:36 +0800

    去掉注视

--- src/main/java/com/nju/risk/manage/domain/UserDO.java
+++ src/main/java/com/nju/risk/manage/domain/UserDO.java
@@ -9,15 +9,6 @@ import java.util.Date;
  * @Company:meituan
  * @date 16/10/31
  */
-
-//`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT '主键',
-//        `user_id` BIGINT UNSIGNED COMMENT '用户ID',
-//        `user_type` TINYINT UNSIGNED NOT NULL COMMENT '用户类型（）',
-//        `user_switch` TINYINT UNSIGNED NOT NULL COMMENT '用户状态（有效、暂停、无效）',
-//        `group_id` SMALLINT COMMENT '所属用户组id',
-//        `gmt_create` datetime NOT NULL COMMENT '创建时间',
-//        `gmt_modified` datetime NOT NULL COMMENT '修改时间',
-//        `data_status` TINYINT UNSIGNED NOT NULL COMMENT '数据有效性',
 public class UserDO {
     //主键
     private Integer id;
