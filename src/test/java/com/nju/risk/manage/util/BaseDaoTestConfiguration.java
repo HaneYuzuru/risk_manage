c61656b8b082f7a789f3dfea003495df2d856025
winsky <winsky94@163.com>
2016-11-08 21:27:45 +0800

    testcase

--- src/test/java/com/nju/risk/manage/util/BaseDaoTestConfiguration.java
+++ src/test/java/com/nju/risk/manage/util/BaseDaoTestConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/applicationContext-dao-test.xml"})
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
 @Transactional
 public class BaseDaoTestConfiguration {
 }
